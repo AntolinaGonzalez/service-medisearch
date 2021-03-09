4dde0822a498e1261bdf9b240393243ba508fd94
Antolina Gonzalez <antolina487@gmail.com>
2021-03-09 20:46:54 -0300

    testing service

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,24 +1,36 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
-import { AppModule } from './../src/app.module';
-
+import { closeInMongodConnection, rootMongooseTestModule } from './mongotest';
+import { MongooseModule } from '@nestjs/mongoose';
+import { DoctorSchema } from './../src/schemas/doctors.schema';
+import { DoctorsService } from './../src/service/doctors.service';
+const app = 'http://localhost:3500'
 describe('AppController (e2e)', () => {
-  let app: INestApplication;
-
+  
+  let service: DoctorsService;
   beforeEach(async () => {
-    const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [AppModule],
+    const module: TestingModule = await Test.createTestingModule({
+      imports: [
+        rootMongooseTestModule(),
+        MongooseModule.forFeature([{ name: 'Doctor', schema: DoctorSchema }]),
+      ],
+      providers: [DoctorsService],
     }).compile();
 
-    app = moduleFixture.createNestApplication();
-    await app.init();
+    service = module.get<DoctorsService>(DoctorsService);
   });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
+  it('should be defined', () => {
+    expect(service).toBeDefined();
+  });
+
+  it('/doctors (GET)', () => {
+    return request(app)
       .get('/')
       .expect(200)
-      .expect('Hello World!');
+  });
+
+  afterAll(async () => {
+    await closeInMongodConnection();
   });
 });
